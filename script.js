468050e17f03336f08d0c2478a0605e085f42594
Rusky <jpah.design@gmail.com>
2020-07-29 23:37:47 -0400

    code working

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ if (nineAm) {
     $("#9am").val(nineAm);
 }
 
-for (var i = 9; i < 17; i++) {
-    // Get the current hour
+for (var i = 9; i <= 17; i++) {
 
+    var today = new Date();
+    //var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();//
     var amPm = "am";
     if (i >= 12) {
         amPm = "pm";
@@ -21,21 +22,64 @@ for (var i = 9; i < 17; i++) {
         hour -= 12;
     }
 
-    
-
     var targetId = "#" + hour + amPm;
+    var currentTime = today.getHours()
 
     var className = "future";
-    // If currentTime > i => class future
-    // else if currentTime < i class past
-    // else class current
-
+    if (currentTime < i) {
+      className = "class future";
+    }else if (currentTime > i) {
+        className  = "class past";
+    }else {
+        className  = "class current";
+    }
     $(targetId).addClass(className);
-
+    
 
 }
 
+var tenAm = localStorage.getItem("#10am");
+if (tenAm) {
+    $("#10am").val(tenAm);
+}
+
+var elevenAm = localStorage.getItem("#11am");
+if (elevenAm) {
+    $("#11am").val(elevenAm);
+}
+
+var twelvePm = localStorage.getItem("#12pm");
+if (twelvePm) {
+    $("#12pm").val(twelvePm);
+}
+
+var onePm = localStorage.getItem("#1pm");
+if (onePm) {
+    $("#1pm").val(onePm);
+}
+
+var twoPm = localStorage.getItem("#2pm");
+if (twoPm) {
+    $("#2pm").val(twoPm);
+}
+
+var threePm = localStorage.getItem("#3pm");
+if (threePm) {
+    $("#12pm").val(threePm);
+}
+
+var fourPm = localStorage.getItem("#4pm");
+if (fourPm) {
+    $("#12pm").val(fourPm);
+}
+
+var fivePm = localStorage.getItem("#5pm");
+if (fivePm) {
+    $("#5pm").val(fivePm);
+}
+
+
 /// Moment JS examples
-console.log(moment().format("MM/DD/YYY"));
+$("#currentDay").text(moment().format("MM/DD/YYYY"));
 console.log(moment().format("HH:mm A"));
 console.log(moment().format("h:mm a"));
